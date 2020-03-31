719ceaa66c99925bc8728a50d4e723e9713e5750
大伟 <811055695@qq.com>
2020-03-31 19:01:00 +0800

    代码清理

--- web/index.php
+++ web/index.php
@@ -1,15 +1,4 @@
 <?php
-//phpinfo();
-//ob_start(); //打开缓冲区
-
-//header("Content-type:json"); //把浏览器重定向到index.php
-//echo "Hellon"; //输出
-
-//ob_end_flush();//输出全部内容到浏览器
-
-//$str = ob_get_contents();//捕获缓存的内容
-//var_dump($str);
-//die;
 define("__ROOT__", dirname(dirname(__FILE__)));
 
 require "../core/Dawei.php";
